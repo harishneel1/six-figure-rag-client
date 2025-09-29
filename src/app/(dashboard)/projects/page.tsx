3b9ae9e575601a08a5cd972b4805d7793e9d52b5
Harish Neel <harishneel@Harishs-MacBook-Pro.local>
2025-09-29 07:41:09 +0530

    user authentication system

---
+++ src/app/(dashboard)/projects/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ProjectsPage() {
+  return (
+    <div>
+      This page shows the list of all the projects associated with teh logged in
+      user
+    </div>
+  );
+}
+
+export default ProjectsPage;
