c3045906c6ff94214b060a6c3b852cf18e338ace
Harish Neel <harishneel@Harishs-MacBook-Pro.local>
2025-09-30 15:04:17 +0530

    feat: added UI components

--- src/app/(dashboard)/projects/page.tsx
+++ src/app/(dashboard)/projects/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProjectsPage() {
   return (
     <div>
-      This page shows the list of all the projects associated with teh logged in
+      This page shows the list of all the projects associated with the logged in
       user
     </div>
   );
